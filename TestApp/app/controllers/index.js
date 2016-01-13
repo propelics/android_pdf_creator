4a29fc4668c13b14e1fdf79776a731d56de4a6d9
Pablo Guevara <pablog.178@gmail.com>
2016-01-13 01:48:22 -0600

    Load mustache.js lib

--- TestApp/app/controllers/index.js
+++ TestApp/app/controllers/index.js
@@ -1,4 +1,16 @@
 var pdfcreator = require("com.propelics.pdfcreator");
+var Mustache = require('mustache');
+
+var view = {
+  title: "Joe",
+  calc: function () {
+    return 2 + 4;
+  }
+};
+
+var output = Mustache.render("{{title}} spends {{calc}}", view);
+
+console.log('output: ' + output);
 
 function doClick(e) {
     alert($.label.text);
