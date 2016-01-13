80728afe2b809ad76067b696e33ff1596c852ed5
Pablo Guevara <pablog.178@gmail.com>
2016-01-13 12:07:15 -0600

    Add example data to parse

--- TestApp/app/controllers/index.js
+++ TestApp/app/controllers/index.js
@@ -6,9 +6,17 @@ function doClick(e) {
 
 $.index.open();
 
+var imageFile = Ti.Filesystem.getFile(Ti.Filesystem.resourcesDirectory, 'image.jpg');
+var imgTemp = Ti.Filesystem.createTempFile();
+imgTemp.write(imageFile.read());
+
 PdfGeneration.generateWithWebView({
-	htmlFile : Ti.Filesystem.getFile(Ti.Filesystem.resourcesDirectory, 'index.html'),
+	htmlFile : Ti.Filesystem.getFile(Ti.Filesystem.resourcesDirectory, 'webview.html'),
 	wrapper : $.index,
+	data : {
+		customerName : 'John Doe',
+		image : imgTemp.resolve().replace('file:', '')
+	},
 	successCallback : function (_data) {
 		console.log('SUCCESS!');
 		console.log('_data: ' + JSON.stringify(_data, null, '\t'));
