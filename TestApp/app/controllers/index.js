c1ce277b443819681abf379186b65aaee5e2fceb
Pablo Guevara <pablog.178@gmail.com>
2016-04-01 13:42:32 -0600

    Add example for page break inside

--- TestApp/app/controllers/index.js
+++ TestApp/app/controllers/index.js
@@ -28,19 +28,32 @@ PdfGeneration.generateWithWebView({
 	}
 });
 
-// PdfGeneration.generatePDFWithTemplate({
-// 	pdfFileName : 'template',
-// 	htmlFile : Ti.Filesystem.getFile(Ti.Filesystem.resourcesDirectory, 'template.html'),
-// 	data : {
-// 		customerName : 'John Doe',
-// 		image : imgTemp.resolve().replace('file:', '')
-// 	},
-// 	successCallback : function (_data) {
-// 		console.log('SUCCESS!');
-// 		console.log('_data: ' + JSON.stringify(_data, null, '\t'));
-// 	},
-// 	failCallback : function (_data) {
-// 		console.log('FAILURE!');
-// 		console.log('_data: ' + JSON.stringify(_data, null, '\t'));
-// 	}
-// });
+PdfGeneration.generatePDFWithTemplate({
+	pdfFileName : 'template',
+	htmlFile : Ti.Filesystem.getFile(Ti.Filesystem.resourcesDirectory, 'template.html'),
+	data : {
+		customerName : 'John Doe',
+		image : imgTemp.resolve().replace('file:', '')
+	},
+	successCallback : function (_data) {
+		console.log('SUCCESS!');
+		console.log('_data: ' + JSON.stringify(_data, null, '\t'));
+	},
+	failCallback : function (_data) {
+		console.log('FAILURE!');
+		console.log('_data: ' + JSON.stringify(_data, null, '\t'));
+	}
+});
+
+PdfGeneration.generatePDFWithTemplate({
+	pdfFileName : 'pageBreakInside',
+	htmlFile : Ti.Filesystem.getFile(Ti.Filesystem.resourcesDirectory, 'pageBreakInside.html')
+	successCallback : function (_data) {
+		console.log('SUCCESS!');
+		console.log('_data: ' + JSON.stringify(_data, null, '\t'));
+	},
+	failCallback : function (_data) {
+		console.log('FAILURE!');
+		console.log('_data: ' + JSON.stringify(_data, null, '\t'));
+	}
+});
